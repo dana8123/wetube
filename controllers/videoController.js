27ca0fb91f69d2ba8fc57b93a5adaa2a3e6f63fc
dana <woworla821@gmail.com>
2020-07-12 17:19:42 +0900

    extends main layout
    메인 레이아웃을 다른 route에도 동일하게 extend하였음.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,6 @@
 export const home = (req, res) => res.render("home");
-export const videos = (req, res) => res.send("videos");
-export const upload = (req, res) => res.send("upload");
-export const videoDetail = (req, res) => res.send("ViedoDetail");
-export const deleteVideo = (req, res) => res.send("Delete Video");
-export const search = (req, res) => res.send("SEARCH");
+export const videos = (req, res) => res.render("videos");
+export const upload = (req, res) => res.render("upload");
+export const videoDetail = (req, res) => res.render("ViedoDetail");
+export const deleteVideo = (req, res) => res.render("Delete Video");
+export const search = (req, res) => res.render("SEARCH");
