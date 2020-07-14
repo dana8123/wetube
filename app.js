33da6d209639b746d236543e05c2a1e6fe1f6f41
dana <woworla821@gmail.com>
2020-07-15 00:34:49 +0900

    add global variable with MiddleWare
    글로벌 변수 선언할 수 있는 미들웨어 파일 생성 및 기존 레이아웃, Partial에 글로벌 변수 추가

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan, { format } from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
+app.use(localsMiddleware);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
