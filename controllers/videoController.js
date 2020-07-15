f16ff03885c0d3dbb4df4700e846ba4343a20a22
dana <woworla821@gmail.com>
2020-07-15 23:30:38 +0900

    add render's second factor
    render 함수에 두번째 인자를 입력하여 로컬변수를 만들어본다..

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,13 @@
-export const home = (req, res) => res.render("home");
-export const videos = (req, res) => res.render("videos");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("ViedoDetail");
-export const deleteVideo = (req, res) => res.render("Delete Video");
-export const search = (req, res) => res.render("SEARCH");
+export const home = (req, res) => res.render("home", { pageTitle: "home" });
+export const videos = (req, res) =>
+	res.render("videos", { pageTitle: "videos" });
+export const upload = (req, res) =>
+	res.render("upload", { pageTitle: "upload" });
+export const videoDetail = (req, res) =>
+	res.render("ViedoDetail", { pageTitle: "video Detail" });
+export const deleteVideo = (req, res) =>
+	res.render("Delete Video", { pageTitle: "delete Video" });
+export const search = (req, res) =>
+	res.render("SEARCH", { pageTitle: "search" });
+export const editVideo = (req, res) =>
+	res.render("editVideo", { pageTitle: "edit video" });
