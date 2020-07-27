de314e01a4e408d7ddee81e32556a59fd49ec593
dana <woworla821@gmail.com>
2020-07-28 00:32:41 +0900

    Search controller
    1. add input box for search at home
    2. make detailes of search controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,13 +1,23 @@
 export const home = (req, res) => res.render("home", { pageTitle: "home" });
+
 export const videos = (req, res) =>
 	res.render("videos", { pageTitle: "videos" });
+
 export const upload = (req, res) =>
 	res.render("upload", { pageTitle: "upload" });
+
 export const videoDetail = (req, res) =>
 	res.render("ViedoDetail", { pageTitle: "video Detail" });
+
 export const deleteVideo = (req, res) =>
 	res.render("Delete Video", { pageTitle: "delete Video" });
-export const search = (req, res) =>
-	res.render("SEARCH", { pageTitle: "search" });
+
+export const search = (req, res) => {
+	const {
+		query: { term: searchingBy },
+	} = req;
+	res.render("SEARCH", { pageTitle: "search", searchingBy });
+};
+
 export const editVideo = (req, res) =>
 	res.render("editVideo", { pageTitle: "edit video" });
