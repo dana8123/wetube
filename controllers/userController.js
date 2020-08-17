0e809bc62ab3bcf4985cf88ca076c633006bd9cf
dana <woworla821@gmail.com>
2020-08-17 23:45:24 +0900

    Update EditProfile.pug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,9 @@
-export const join = (req, res) => res.render("Join");
-export const login = (req, res) => res.render("Login");
+export const join = (req, res) => res.render("Join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("Login", { pageTitle: "Login" });
 export const logout = (req, res) => res.render("Logout");
 export const users = (req, res) => res.render("users");
 export const userDetail = (req, res) => res.render("user Detail");
-export const editProfile = (req, res) => res.render("Edit Profile");
-export const changePassword = (req, res) => res.render("Change password");
+export const editProfile = (req, res) =>
+	res.render("editProfile", { pageTitle: "editProfile" });
+export const changePassword = (req, res) =>
+	res.render("changePassword", { pageTitle: "changePssword" });
