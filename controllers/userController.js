057bc059a80ea494d933a6425120beb974b1e40d
dana <woworla821@gmail.com>
2020-08-23 23:16:33 +0900

    Add mixin on search.PUG / edit user controller
    1. search.PUG에 동영상 mixin추가
    2. user controller를 수정하여 login 로직 구성

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,29 @@
-export const join = (req, res) => res.render("Join", { pageTitle: "Join" });
-export const login = (req, res) => res.render("Login", { pageTitle: "Login" });
+import routes from "../routes.js";
+
+export const getJoin = (req, res) => {
+	res.render("Join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+	const {
+		body: { name, email, password, password2 },
+	} = req;
+	if (password !== password2) {
+		res.status(400);
+	} else {
+		//toDo register User, also log user in
+		res.redirect(routes.home);
+	}
+	res.render("join", { pageTitle: "join" });
+};
+
+export const getLogin = (req, res) => {
+	res.render("Login", { pageTitle: "Login" });
+};
+export const postLogin = (req, res) => {
+	res.redirect(routes.home);
+};
+
 export const logout = (req, res) => res.render("Logout");
 export const users = (req, res) => res.render("users");
 export const userDetail = (req, res) => res.render("user Detail");
